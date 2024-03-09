a2b37a45f6e9f2d885b84aa14013ae23eb0a464b
DESKTOP-83N2NSV\Admino <camtv.frmobile@gmail.com>
2024-03-09 17:57:39 +0700

    add file

---
+++ ClearChromeCache/frmMain.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace ClearChromeCache
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.buttonBrowser = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonBrowser
+            // 
+            this.buttonBrowser.Location = new System.Drawing.Point(12, 12);
+            this.buttonBrowser.Name = "buttonBrowser";
+            this.buttonBrowser.Size = new System.Drawing.Size(150, 30);
+            this.buttonBrowser.TabIndex = 0;
+            this.buttonBrowser.Text = "Browser UserData";
+            this.buttonBrowser.UseVisualStyleBackColor = true;
+            this.buttonBrowser.Click += new System.EventHandler(this.buttonBrowser_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(12, 237);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(41, 15);
+            this.labelStatus.TabIndex = 1;
+            this.labelStatus.Text = "Status";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonBrowser);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "frmMain";
+            this.Text = "Clear Chrome Cache";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonBrowser;
+        private System.Windows.Forms.Label labelStatus;
+    }
+}
+
---
+++ ClearChromeCache/frmMain.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClearChromeCache
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void buttonBrowser_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string folder = dialog.SelectedPath;
+
+                Task.Run(() =>
+                {
+                    string[] defaults = Directory.GetDirectories(folder, "Default", SearchOption.AllDirectories);
+                    foreach (var profile in defaults)
+                    {
+                        labelStatus.Invoke((MethodInvoker)delegate { labelStatus.Text = profile; });
+                        string cache = profile + "\\Cache\\Cache_Data";
+                        string code_cache1 = profile + "\\Code Cache\\js";
+                        string code_cache2 = profile + "\\Code Cache\\wasm";
+                        string gpu_cache = profile + "\\GPUCache";
+                        if (Directory.Exists(cache))
+                        {
+                            labelStatus.Invoke((MethodInvoker)delegate { labelStatus.Text = "Clear Cache"; });
+                            Directory.Delete(cache, true);
+                        }
+                        if (Directory.Exists(code_cache1))
+                        {
+                            labelStatus.Invoke((MethodInvoker)delegate { labelStatus.Text = "Code Cache js"; });
+                            Directory.Delete(code_cache1, true);
+                        }
+                        if (Directory.Exists(code_cache2))
+                        {
+                            labelStatus.Invoke((MethodInvoker)delegate { labelStatus.Text = "Code Cache wasm"; });
+                            Directory.Delete(code_cache2, true);
+                        }
+                        if (Directory.Exists(gpu_cache))
+                        {
+                            labelStatus.Invoke((MethodInvoker)delegate { labelStatus.Text = "GPUCache"; });
+                            foreach (var item in Directory.GetFiles(gpu_cache))
+                            {
+                                File.Delete(item);
+                            }
+                        }
+                    }
+                    string[] profiles = Directory.GetDirectories(folder, "Profile ", SearchOption.AllDirectories);
+                    foreach (var profile in profiles)
+                    {
+                        labelStatus.Invoke((MethodInvoker)delegate { labelStatus.Text = profile; });
+                        string cache = profile + "\\Cache\\Cache_Data";
+                        string code_cache1 = profile + "\\Code Cache\\js";
+                        string code_cache2 = profile + "\\Code Cache\\wasm";
+                        string gpu_cache = profile + "\\GPUCache";
+                        if (Directory.Exists(cache))
+                        {
+                            labelStatus.Invoke((MethodInvoker)delegate { labelStatus.Text = "Clear Cache"; });
+                            Directory.Delete(cache, true);
+                        }
+                        if (Directory.Exists(code_cache1))
+                        {
+                            labelStatus.Invoke((MethodInvoker)delegate { labelStatus.Text = "Code Cache js"; });
+                            Directory.Delete(code_cache1, true);
+                        }
+                        if (Directory.Exists(code_cache2))
+                        {
+                            labelStatus.Invoke((MethodInvoker)delegate { labelStatus.Text = "Code Cache wasm"; });
+                            Directory.Delete(code_cache2, true);
+                        }
+                        if (Directory.Exists(gpu_cache))
+                        {
+                            labelStatus.Invoke((MethodInvoker)delegate { labelStatus.Text = "GPUCache"; });
+                            foreach (var item in Directory.GetFiles(gpu_cache))
+                            {
+                                File.Delete(item);
+                            }
+                        }
+                    }
+                    labelStatus.Invoke((MethodInvoker)delegate { labelStatus.Text = "Clear Done"; });
+                    MessageBox.Show("Clear Done");
+                });
+            }
+        }
+    }
+}
